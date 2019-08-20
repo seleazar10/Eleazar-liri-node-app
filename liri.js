9fa6b51a03c0ec97c49c14a2cc2b47ac0afe819d
Stama Eleazar <eleoatam10@gmail.com>
2019-08-20 14:50:51 -0500

    spotify works

--- liri.js
+++ liri.js
@@ -1,18 +1,16 @@
-// require("dotenv").config();
-// var keys = require("./keys");
+require("dotenv").config();
+var keys = require("./keys");
 
 var fs = require('fs');
-
-// var request = require("request");
 
 var moment = require("moment");
 
 var axios = require('axios');
 
-// var Spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 
 
@@ -22,60 +20,60 @@ var input = process.argv[3]
 
 //command switch
 
-if(operator === 'concert-this'){
-  
+if (operator === 'concert-this') {
+
   concerts()
-  
-}else if(operator === 'spotify-this-song'){
-  
-  spotify()
-  
-}else if(operator === 'movie-this'){
-  
+
+} else if (operator === 'spotify-this-song') {
+
+  spotMusic(input)
+
+} else if (operator === 'movie-this') {
+
   movies()
-  
-}else if(operator === 'do-what-it-says'){
-  
+
+} else if (operator === 'do-what-it-says') {
+
   performTask()
-  
-}else{
+
+} else {
   console.log('Please choose a command')
 }
 
 
 //run function for concert-this
 
-function concerts(){
+function concerts() {
 
   var concertsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
 
   axios
     .get(concertsUrl)
     .then(function (concert) {
-    // handle success
-
-    
-    for (var i=0; i<concert.data.length; i++){
-      var conData = concert.data[i];
-
-      var conDate = moment(conData.datetime).format("MM/DD/YYYY");
-
-      // console.log(conData)
-
-      console.log(conData.venue.name)
-      console.log(conData.venue.city)
-      console.log(conData.venue.country)
-      console.log(conDate)
-      console.log('-----------------------------')
-
-    }
+      // handle success
 
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+      for (var i = 0; i < concert.data.length; i++) {
+        var conData = concert.data[i];
+
+        var conDate = moment(conData.datetime).format("MM/DD/YYYY");
+
+        // console.log(conData)
+
+        console.log(conData.venue.name)
+        console.log(conData.venue.city)
+        console.log(conData.venue.country)
+        console.log(conDate)
+        console.log('-----------------------------')
+
+      }
+
+
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 
 
 
@@ -83,54 +81,90 @@ function concerts(){
 }
 
 
-function movies(){
+function movies() {
 
   var moviesUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
   axios.get(moviesUrl)
-  .then(function (movies) {
-    // handle success
+    .then(function (movies) {
+      // handle success
 
-    var movData = movies.data
-    // console.log(movData);
-    console.log('title: ' + movData.Title);
-    console.log(movData.Year);
-    console.log(movData.Rated);
-    console.log('IMDB Rating: '+ movData.imdbRating);
-    console.log('Rotten Tomatoes Rating: '+ movData.Ratings[2]); //need yo stringify
-    console.log('Country Produced: '+ movData.Country);
-    console.log('Languages: '+ movData.Language);
-    console.log('Plots: '+ movData.Plot);
-    console.log('Actors: '+ movData.Actors);
+      var movData = movies.data
+      // console.log(movData);
+      console.log('title: ' + movData.Title);
+      console.log(movData.Year);
+      console.log(movData.Rated);
+      console.log('IMDB Rating: ' + movData.imdbRating);
+      console.log('Rotten Tomatoes Rating: ' + movData.Ratings[2]); //need to stringify
+      console.log('Country Produced: ' + movData.Country);
+      console.log('Languages: ' + movData.Language);
+      console.log('Plots: ' + movData.Plot);
+      console.log('Actors: ' + movData.Actors);
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 }
 
 
-function performTask(){
+function performTask() {
   var textToInput = ""
 
-  fs.readFile("random.txt", "utf8", function(error, data) {
+  fs.readFile("random.txt", "utf8", function (error, data) {
 
     // error output
     if (error) {
       return console.log(error);
     }
-  
+
     //print data
     // console.log(data);
-  
+
 
     //split data to make it easier to read
     var dataSplit = data.split(",");
     console.log(dataSplit);
-  
+    var first = dataSplit[0]
+    console.log(first)
+    var second = dataSplit[1]
+    console.log(second)
+
+    spotify(second)
+
   });
 }
+
+function spotMusic(){
+  console.log('works')
+  spotify.search({ type: 'track', query: input }, function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+    
+        // console.log(data);  
+
+        var neededData = data.tracks.items
+
+
+        // console.log(neededData)
+
+        for(var i=0; i<5; i++){
+          console.log(neededData[i].artists[0].name)
+          console.log(neededData[i].name)      
+          console.log(neededData[i].preview_url)        
+          console.log(neededData[i].album.name)
+          console.log('---------------------------------')
+
+        }
+    
+      })
+}
+
+
+
+
 
 
 
