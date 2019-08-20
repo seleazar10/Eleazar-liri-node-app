b817f9baac271ce49c60b69a4667eba623faeecc
Stama Eleazar <eleoatam10@gmail.com>
2019-08-19 21:28:20 -0500

    concert-this works

--- liri.js
+++ liri.js
@@ -1,12 +1,18 @@
-require("dotenv").config();
-var keys = require("./keys");
+// require("dotenv").config();
+// var keys = require("./keys");
 
-var fs = require('fs')
+// var fs = require('fs');
 
-var axios = require('axios')
+// var request = require("request");
+
+var moment = require("moment");
+
+var axios = require('axios');
+
+// var Spotify = require("node-spotify-api");
 
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 
 
@@ -39,85 +45,40 @@ if(operator === 'concert-this'){
 
 function concerts(){
 
-  var concertsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+  var concertsUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
 
-  
+  axios
+    .get(concertsUrl)
+    .then(function (concert) {
+    // handle success
 
-  console.log('Name of the venue:')
-  console.log('Location of the venue:')
-  console.log('Date:')
+    
+    for (var i=0; i<concert.data.length; i++){
+      var conData = concert.data[i];
+
+      var conDate = moment(conData.datetime).format("MM/DD/YYYY");
+
+      // console.log(conData)
+
+      console.log(conData.venue.name)
+      console.log(conData.venue.city)
+      console.log(conData.venue.country)
+      console.log(conDate)
+      console.log('-----------------------------')
+
+    }
+
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
+
+
+  // console.log('Name of the venue:')
+  // console.log('Location of the venue:')
+  // console.log('Date:')
 }
 
-
-// require("dotenv").config();
-// //Request is designed to be the simplest way possible to make http calls
-// var request = require("request");
-// //require to read an write file systems
-// var fs = require("fs");
-// //require to run moment js through npm
-// var moment = require("moment");
-// // code required to import from keys.js
-// var keys = require("./keys.js");
-// //require to run spotify api thorough npm
-// var Spotify = require("node-spotify-api");
-// var axios = require("axios");
-// //should be able to access  keys information to access spotify
-// var spotify = new Spotify(keys.spotify);
-// //Variable assigned the will accept arguments being passed
-// var instructions = process.argv[2];
-// var parameter = process.argv[3];
-// //Execute function
-// inputs(instructions, parameter);
-// //Switch Statments
-// function inputs(instructions, parameter) {
-//  switch (instructions) {
-//    case "concert-this":
-//      newConcert(parameter);
-//      break;
-//    case "spotify-this-song":
-//      spotSong(parameter);
-//      break;
-//    case "movie-this":
-//      movieInfo(parameter);
-//      break;
-//    case "do-what-it-says":
-//      getRandom(parameter);
-//      break;
-//    default:
-//      console.log(
-//        "Please the following options: \nconcert-this \nspotify-this-song \nmovie-this"
-//      );
-//  }
-// }
-
-
-// var newConcert= function(artist){
-//    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-//    axios.get(queryURL)
-//   .then(function (artist) {
-//     // handle success
-//     for (var i =0; i<artist.data.length; i++ ){
-//         var artists = artist.data[i]
-//         console.log(artists);
-
-
-//         console.log(artists.venue.name)
-//         console.log(artists.venue.city)
-//         console.log(artists.venue.region)
-
-        
-
-//     }
-    
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-  
-
-   
-
-    
-// }
